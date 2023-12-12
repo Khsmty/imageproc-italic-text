2528b2ab8cbd009395d98ef9d99f0cf47ca8c652
Khsmty <me@khsmty.com>
2023-12-12 15:20:43 +0900

    refactor: use imageproc

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,29 @@
 use image::{imageops::overlay, DynamicImage, Rgba, RgbaImage};
-use imageproc::geometric_transformations::{warp_into, Interpolation, Projection};
-use imageutil::text::{empty_resolver, Fonts};
+use imageproc::{
+    drawing::draw_text_mut,
+    geometric_transformations::{warp_into, Interpolation, Projection},
+};
 use once_cell::sync::Lazy;
 use rusttype::{Font, Scale};
 
 const INTER: Lazy<Font> =
     Lazy::new(|| Font::try_from_bytes(include_bytes!("../fonts/Inter-Regular.ttf")).unwrap());
-const FONT_SETTING: Lazy<Vec<Font<'static>>> = Lazy::new(|| vec![INTER.clone()]);
 
 #[tokio::main]
 async fn main() {
     let mut img = DynamicImage::new_rgba8(400, 100);
-    let fonts = Fonts::new(FONT_SETTING.clone());
 
     let mut text_image = DynamicImage::new_rgba8(300, 80);
     let mut italic_text_image = RgbaImage::new(300, 80);
-    fonts
-        .write_to(
-            &mut text_image,
-            "Hello, World!",
-            Scale::uniform(60.0),
-            Rgba::from([0, 0, 0, 255]),
-            0,
-            0,
-            empty_resolver,
-        )
-        .await;
+    draw_text_mut(
+        &mut text_image,
+        Rgba::from([0, 0, 0, 255]),
+        0,
+        0,
+        Scale::uniform(60.0),
+        &INTER.clone(),
+        "Hello, World!",
+    );
 
     let matrix = Projection::from_matrix([
         1_f32,    // 固定
